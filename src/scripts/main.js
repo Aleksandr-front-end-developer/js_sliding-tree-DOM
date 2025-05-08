b29fe1c3a83d0a52cdfe7813be4ee870904de82f
Aleksandr-front-end-developer <oleksandr.danyliuk.88@gmail.com>
2025-05-08 13:20:46 +0300

    have done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+
+tree.querySelectorAll('ul').forEach((item) => {
+  const li = item.parentNode;
+  const childNodes = Array.from(li.childNodes);
+
+  childNodes.forEach((node) => {
+    if (node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '') {
+      const span = document.createElement('span');
+      const nextEl = span.nextElementSibling;
+
+      span.textContent = node.textContent;
+      li.replaceChild(span, node);
+
+      nextEl.style.display = 'block';
+
+      span.addEventListener('click', (e) => {
+        nextEl.style.display =
+          nextEl.style.display === 'none' || nextEl.style.display === ''
+            ? 'block'
+            : 'none';
+      });
+    }
+  });
+});
